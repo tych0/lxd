5ac6e013a2fa703eea8191f7a9b2e59cf8b77a06
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-11-12 00:33:11 -0600

    lxd - add basic cert gen code
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- lxc/main.go
+++ lxc/main.go
@@ -20,6 +20,39 @@ func main() {
 var verbose = gnuflag.Bool("v", false, "Enables verbose mode.")
 var debug = gnuflag.Bool("debug", false, "Enables debug mode.")
 
+func create_cert() error {
+	homedir := os.Getenv("HOME")
+	if homedir == "" {
+		return fmt.Errorf("Failed to find homedir")
+	}
+	certf := fmt.Sprintf("%s/.config/lxd/%s", homedir, "cert.pem")
+	keyf := fmt.Sprintf("%s/.config/lxd/%s", homedir, "key.pem")
+	lxd.Debugf("looking for existing certificates: %s %s", certf, keyf)
+
+	_, err := os.Stat(certf)
+	_, err2 := os.Stat(keyf)
+	if err == nil && err2 == nil {
+		lxd.Debugf("certificates already exist")
+		return nil
+	}
+	if err == nil {
+		lxd.Debugf("%s already exists", certf)
+		return err2
+	}
+	if err2 == nil {
+		lxd.Debugf("%s already exists", keyf)
+		return err
+	}
+	dir := fmt.Sprintf("%s/.config/lxd", homedir)
+	err = os.MkdirAll(dir, 0750)
+	if err != nil {
+		return err
+	}
+
+	lxd.Debugf("creating cert: %s %s", certf, keyf)
+	return lxd.GenCert(certf, keyf)
+}
+
 func run() error {
 	if len(os.Args) == 2 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
 		os.Args[1] = "help"
@@ -45,6 +78,12 @@ func run() error {
 		lxd.SetLogger(log.New(os.Stderr, "", log.LstdFlags))
 		lxd.SetDebug(*debug)
 	}
+
+	err := create_cert()
+	if err != nil {
+		return err
+	}
+
 	return cmd.run(gnuflag.Args())
 }
 
