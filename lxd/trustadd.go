beec811a549851c7a8a3193998f374e9e7c22ecb
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-11-12 22:27:52 -0600

    lxd: store client certificate from trust/add
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- lxd/trustadd.go
+++ lxd/trustadd.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"os"
 
 	"github.com/lxc/lxd"
 )
@@ -21,6 +21,19 @@ func (d *Daemon) serveTrustAdd(w http.ResponseWriter, r *http.Request) {
 	for i := range r.TLS.PeerCertificates {
 		peercert := &r.TLS.PeerCertificates[i]
 		fmt.Printf("PeerCertificate %d : %d\n", i, peercert)
-		// TODO: save this certificate
+		// TODO - do we need to sanity-check the server name to avoid arbitrary writes to fs?
+		dirname := lxd.VarPath("clientcerts")
+		err := os.MkdirAll(dirname, 0755)
+		filename := fmt.Sprintf("%s/%s", dirname, r.TLS.ServerName)
+		lxd.Debugf("filename is %s", filename)
+		certout, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+		if err != nil {
+			lxd.Debugf("Error opening file for  client certificate: %q", err)
+			continue
+		}
+		_, err = certout.Write(r.TLS.PeerCertificates[i].Raw)
+		if err !=  nil {
+			lxd.Debugf("Error writing client certificate: %q", err)
+		}
 	}
 }
